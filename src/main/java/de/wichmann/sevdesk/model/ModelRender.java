ae1a557b58303f284e3b17bd7b20d15a80547458
Marvin Wichmann <marvin.wichmann@unic.com>
2019-05-03 14:49:27 +0200

    Added missing accessors

--- src/main/java/de/wichmann/sevdesk/model/ModelRender.java
+++ src/main/java/de/wichmann/sevdesk/model/ModelRender.java
@@ -10,4 +10,14 @@ public class ModelRender
 {
 	@JsonProperty(value = "pdfUrl")
 	private String pdfUrl;
+
+	public String getPdfUrl()
+	{
+		return pdfUrl;
+	}
+
+	public void setPdfUrl(String pdfUrl)
+	{
+		this.pdfUrl = pdfUrl;
+	}
 }
